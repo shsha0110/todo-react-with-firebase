427b33779f3f5c55cf5531811f3e3ce4407575c2
shsha0110 <shsh0201@snu.ac.kr>
2023-05-28 01:17:02 +0900

    update: firebase initilaization
    update: firebase initilaization

--- todo/src/firebase/index.js
+++ todo/src/firebase/index.js
@@ -12,6 +12,10 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
