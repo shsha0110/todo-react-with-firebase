45a506ca610e7c61333a2f4dfa1ed17eb7bb9943
shsha0110 <shsh0201@snu.ac.kr>
2023-05-28 13:18:59 +0900

    update: logic of stoer user data
    update: logic of store user data

--- todo/src/pages/auth/signin.js
+++ todo/src/pages/auth/signin.js
@@ -1,86 +1,72 @@
 import { useRouter } from "next/router";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { useEffect, useState } from 'react';
-import { db } from "@/firebase/index.js";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from '@/firebase/index.js';
+
+async function createUser(uid, data) {
+  const userRef = doc(db, 'users', uid);
+  const docSnap = await getDoc(userRef);
+
+  if (!docSnap.exists()) { // Check if the user doesn't exist
+    await setDoc(userRef, { uid, ...data }); // Only then create the user
+  }
+}
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    createUser(user.uid, { name: user.displayName, email: user.email });
+  }
+});
 
 export default function Signin() {
-    const router = useRouter();
-    const { data : session } = useSession();
-    const [user, setUser] = useState(null);
-    
-    useEffect(() => {
-        if(session){
-            const docRef = db.collection("users").doc(session.user.email);
-            
-            docRef.get().then((doc) => {
-                if (doc.exists) {
-                    setUser(doc.data());
-                } else {
-                    db.collection("users").doc(session.user.email).set({
-                        name: session.user.name,
-                        email: session.user.email
-                    })
-                    .then(() => {
-                        console.log("Document successfully written!");
-                        setUser({
-                          name: session.user.name,
-                          email: session.user.email
-                        });
-                    })
-                    .catch((error) => {
-                        console.error("Error writing document: ", error);
-                    });
-                }
-            }).catch((error) => {
-                console.log("Error getting document:", error);
-            });
-        }
-    }, [session]);
+  const router = useRouter();
+  const { data: session } = useSession();
 
-    return (
-        <div className="flex justify-center h-screen">
-          {session ? (
-            <div className="grid m-auto text-center">
-              <div className="m-4">Signed in as {user ? user.name : 'Loading...'}</div>
-              <button
-                className={`w-40
-                          justify-self-center
-                          p-1 mb-4
-                        bg-blue-500 text-white
-                          border border-blue-500 rounded
-                        hover:bg-white hover:text-blue-500`}
-                onClick={ () => router.push("/")}
-              >
-                Go to Home
-              </button>
-              <button
-                className={`w-40
-                          justify-self-center
-                          p-1 mb-4
-                        text-blue-500
-                          border border-blue-500 rounded
-                        hover:bg-white hover:text-blue-500`}
-                onClick={() => signOut()}
-              >
-                Sign out
-              </button>
-            </div>
-          ) : (
-            <div className="grid m-auto text-center">
-              <div className="m-4">Not signed in</div>
-              <button
-                className={`w-40
-                          justify-self-center
-                          p-1 mb-4
-                        bg-blue-500 text-white
-                          border border-blue-500 rounded
-                        hover:bg-white hover:text-blue-500`}
-                onClick={async() => await signIn()}
-              >
-                Sign in
-              </button>
-            </div>
-          )}
+  return (
+    <div className="flex justify-center h-screen">
+      {session ? (
+        <div className="grid m-auto text-center">
+          <div className="m-4">Signed in as {session.user.name}</div>
+          <button
+            className={`w-40
+                      justify-self-center
+                      p-1 mb-4
+                    bg-blue-500 text-white
+                      border border-blue-500 rounded
+                    hover:bg-white hover:text-blue-500`}
+            onClick={() => router.push("/")}
+          >
+            Go to Home
+          </button>
+          <button
+            className={`w-40
+                      justify-self-center
+                      p-1 mb-4
+                    text-blue-500
+                      border border-blue-500 rounded
+                    hover:bg-white hover:text-blue-500`}
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
         </div>
-    );
+      ) : (
+        <div className="grid m-auto text-center">
+          <div className="m-4">Not signed in</div>
+          <button
+            className={`w-40
+                      justify-self-center
+                      p-1 mb-4
+                    bg-blue-500 text-white
+                      border border-blue-500 rounded
+                    hover:bg-white hover:text-blue-500`}
+            onClick={() => signIn()}
+          >
+            Sign in
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
