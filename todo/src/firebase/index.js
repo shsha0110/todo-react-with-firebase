70c1789ab515af1b08424b2a443eb03e3afa3632
shsha0110 <shsh0201@snu.ac.kr>
2023-06-03 22:34:12 +0900

    ?
    ?

--- todo/src/firebase/index.js
+++ todo/src/firebase/index.js
@@ -16,4 +16,15 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
+//Email 회원가입
+export const signupEmail = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+//Email 로그인
+export const loginEmail = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+
 export { auth, db };
